ebb6b80d00587a12e1b6fe6cfd54caad08f36104
Brandon Geren <brandongeren@gmail.com>
2019-06-06 17:09:36 -0500

    Implement GameState interface

--- src/app/ygo/game.ts
+++ src/app/ygo/game.ts
@@ -1,5 +1,50 @@
 export interface IGameState {
+  // TODO: String might not be the best data structure to represent a player username
 
+  // the players map uses player usernames to their player gamestate 
+  players: Map<String, IPlayerGameState>,
+  turnPlayer: String,
+  // TODO: make phases into a real type
+  phase: String,
+
+  // lifepoints is a map from player usernames to their lifepoints as a number
+  // TODO: make this an integer type
+  lifepoints: Map<String, number>,
+
+  // target is a map from player usernames to the card they are pointing at
+  // useful for cards like mystical space typhoon that require selecting a target
+  target: Map<String, IZone>,
+
+  // TODO: create extra monster zones here
+}
+
+export interface IPlayerGameState {
+  hand: Array<GameCard>,
+  banish: Array<GameCard>,
+  deck: Array<GameCard>,
+  extra: Array<GameCard>,
+  // graveyard is of type ICard because it is always public information
+  grave: Array<ICard>,
+  s1: IZone,
+  s2: IZone,
+  s3: IZone,
+  s4: IZone,
+  s5: IZone,
+  m1: IMonsterZone,
+  m2: IMonsterZone,
+  m3: IMonsterZone,
+  m4: IMonsterZone,
+  m5: IMonsterZone,
+}
+
+export interface IZone {
+  card?: GameCard,
+  faceup: Boolean,
+}
+
+export interface IMonsterZone extends IZone {
+  // atkPosition is true if the monster is in atk position
+  atkPosition: Boolean,
 }
 
 export type GameCard = ICard|IUnknownCard;
